5e91e0310caff24e49f014419372dba218d06b24
dev-miguel-resources <ingenieromiguelch@gmail.com>
2020-12-15 15:35:06 -0300

    dark mode react.js

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import Splash from "./Components/SplashScreen";
+import { ThemeProvider } from "styled-components";
+
+const LightTheme = {
+  pageBackground: "white",
+  titleColor: "#dc658b",
+  tagLineColor: "black"
+};
+
+const DarkTheme = {
+  pageBackground: "#282c36",
+  titleColor: "lightpink",
+  tagLineColor: "lavender"
+}
+
+const themes = {
+  light: LightTheme,
+  dark: DarkTheme,
+}
+
+
+function App() {
+  const [theme, setTheme] = useState("light")
+  return (
+    <ThemeProvider theme={themes[theme]}>
+      <Splash theme={theme} setTheme={setTheme} />
+    </ThemeProvider>
+  );
+}
+
+export default App;
